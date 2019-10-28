3e31205fdb83e67d0b6ccc8fec640533bedea582
Leander Mathisen <leander@Leanders-MacBook-Pro.local>
2019-10-27 23:25:32 -0400

    *voice_commands*

--- Virtual-Assistant.py
+++ Virtual-Assistant.py
@@ -2,7 +2,9 @@ import pyowm
 from datetime import datetime, date
 import calendar
 import time
+import speech_recognition as sr
 
+r = sr.Recognizer()
 monday_reminders = []
 tuesday_reminders = []
 wednesday_reminders = []
@@ -10,7 +12,7 @@ thursday_reminders = []
 friday_reminders = []
 saturday_reminders = []
 sunday_reminders = []
-
+print(int("one"))
 def get_weather():
     owm = pyowm.OWM("653ae96c89b55a1cccf01e7243d6b94b")
     location = owm.weather_at_place("Toronto")
@@ -58,26 +60,32 @@ def get_date():
     print("The date is the " + day + day_sub + " of " + month + ", " + year + ".")
 
 def make_reminder():
-    command = input("What reminder would you like me to set?: ")
-    if command[3:9] == "Monday":
+    with sr.Microphone() as source:
+        print("What reminder would you like me to set?: ")
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+    except:
+        print("Could not recognize your voice")
+    if command[3:9] == "monday":
         monday_reminders.append(command[23:])
         print("Will remind you to " + monday_reminders[-1] + " on Monday.")
-    elif command[3:10] == "Tuesday":
+    elif command[3:10] == "tuesday":
         tuesday_reminders.append(command[24:])
         print("Will remind you to " + tuesday_reminders[-1] + " on Tuesday.")
-    elif command[3:12] == "Wednesday":
+    elif command[3:12] == "wednesday":
         wednesday_reminders.append(command[26:])
         print("Will remind you to " + wednesday_reminders[-1] + " on Wednesday.")
-    elif command[3:11] == "Thursday":
+    elif command[3:11] == "thursday":
         thursday_reminders.append(command[25:])
         print("Will remind you to " + thursday_reminders[-1] + " on Thursday.")
-    elif command[3:9] == "Friday":
+    elif command[3:9] == "friday":
         friday_reminders.append(command[23:])
         print("Will remind you to " + friday_reminders[-1] + " on Friday.")
-    elif command[3:11] == "Saturday":
+    elif command[3:11] == "saturday":
         saturday_reminders.append(command[25:])
         print("Will remind you to " + saturday_reminders[-1] + " on Saturday.")
-    elif command[3:9] == "Sunday":
+    elif command[3:9] == "sunday":
         sunday_reminders.append(command[23:])
         print("Will remind you to " + sunday_reminders[-1] + " on Sunday.")
 
@@ -107,99 +115,147 @@ def show_reminders_today():
             print(sunday_reminders)
 
 def show_reminders_date():
-    if "Monday" in command:
+    if "monday" in command:
         for task in monday_reminders:
             print(task)
-    elif "Tuesday" in command:
+    elif "tuesday" in command:
         for task in tuesday_reminders:
             print(task)
-    elif "Wednesday" in command:
+    elif "wednesday" in command:
         for task in wednesday_reminders:
             print(task)
-    elif "Thursday" in command:
+    elif "thursday" in command:
         for task in thursday_reminders:
             print(task)
-    elif "Friday" in command:
+    elif "friday" in command:
         for task in friday_reminders:
             print(task)
-    elif "Saturday" in command:
+    elif "saturday" in command:
         for task in saturday_reminders:
             print(task)
-    elif "Sunday" in command:
+    elif "sunday" in command:
         for task in sunday_reminders:
             print(sunday_reminders)
 
 def remove_task():
     task_number = 0
-    command = input("On what day would you like to remove the task for: ")
-    if command == "Monday":
+    with sr.Microphone() as source:
+        print("On what day would you like me to remove the task for: ")
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+    except:
+        print("Could not recognize your voice")
+    if command == "monday":
         for task in monday_reminders:
             task_number = task_number + 1
             print(str(task_number) + ") " + task)
-        task_deleted = input("Which task would you like to remove: ")
+        with sr.Microphone() as source:
+            print("Which task would you like to remove: ")
+            audio = r.listen(source)
+        try:
+            task_deleted = r.recognize_google(audio)
+        except:
+            print("Could not recognize your voice")
         if len(monday_reminders) >= int(task_deleted):
             print("I have removed task " + task_deleted + " from your reminders on Monday.")
             monday_reminders.pop(int(task_deleted) - 1)
         else:
             print("That task is not in range of Monday reminders.")
-    elif command == "Tuesday":
+    elif command == "tuesday":
         for task in tuesday_reminders:
             task_number = task_number + 1
             print(str(task_number) + ") " + task)
-        task_deleted = input("Which task would you like to remove: ")
+        with sr.Microphone() as source:
+            print("Which task would you like to remove: ")
+            audio = r.listen(source)
+        try:
+            task_deleted = r.recognize_google(audio)
+        except:
+            print("Could not recognize your voice")
         if len(tuesday_reminders) >= int(task_deleted):
             print("I have removed task " + task_deleted + " from your reminders on Tuesday.")
             tuesday_reminders.pop(int(task_deleted) - 1)
         else:
             print("That task is not in range of Tuesday reminders.")
-    elif command == "Wednesday":
+    elif command == "wednesday":
         for task in wednesday_reminders:
             task_number = task_number + 1
             print(str(task_number) + ") " + task)
-        task_deleted = input("Which task would you like to remove: ")
+        with sr.Microphone() as source:
+            print("Which task would you like to remove: ")
+            audio = r.listen(source)
+        try:
+            task_deleted = r.recognize_google(audio)
+        except:
+            print("Could not recognize your voice")
         if len(wednesday_reminders) >= int(task_deleted):
             print("I have removed task " + task_deleted + " from your reminders on Wednesday.")
-            wednesday_reminders.pop(int(task_deleted - 1))
+            wednesday_reminders.pop(int(task_deleted) - 1)
         else:
             print("That task is not in range of Wednesday reminders.")
-    elif command == "Thursday":
+    elif command == "thursday":
         for task in thursday_reminders:
             task_number = task_number + 1
             print(str(task_number) + ") " + task)
-        task_deleted = input("Which task would you like to remove: ")
+        with sr.Microphone() as source:
+            print("Which task would you like to remove: ")
+            audio = r.listen(source)
+        try:
+            task_deleted = r.recognize_google(audio)
+        except:
+            print("Could not recognize your voice")
         if len(thursday_reminders) >= int(task_deleted):
             print("I have removed task " + task_deleted + " from your reminders on Thursday.")
-            thursday_reminders.pop(int(task_deleted - 1))
+            thursday_reminders.pop(int(task_deleted) - 1)
         else:
             print("That task is not in range of Thursday reminders.")
-    elif command == "Friday":
+    elif command == "friday":
         for task in friday_reminders:
             task_number = task_number + 1
             print(str(task_number) + ") " + task)
-        task_deleted = input("Which task would you like to remove: ")
+        with sr.Microphone() as source:
+            print("Which task would you like to remove: ")
+            audio = r.listen(source)
+        try:
+            task_deleted = r.recognize_google(audio)
+        except:
+            print("Could not recognize your voice")
         if len(friday_reminders) >= int(task_deleted):
             print("I have removed task " + task_deleted + " from your reminders on Friday.")
-            friday_reminders.pop(int(task_deleted - 1))
+            friday_reminders.pop(int(task_deleted) - 1)
         else:
             print("That task is not in range of Friday reminders.")
-    elif command == "Saturday":
+    elif command == "saturday":
         for task in saturday_reminders:
             task_number = task_number + 1
             print(str(task_number) + ") " + task)
-        task_deleted = input("Which task would you like to remove: ")
+        with sr.Microphone() as source:
+            print("Which task would you like to remove: ")
+            audio = r.listen(source)
+        try:
+            task_deleted = r.recognize_google(audio)
+        except:
+            print("Could not recognize your voice")
         if len(saturday_reminders) >= int(task_deleted):
             print("I have removed task " + task_deleted + " from your reminders on Saturday.")
-            saturday_reminders.pop(int(task_deleted - 1))
+            saturday_reminders.pop(int(task_deleted) - 1)
         else:
             print("That task is not in range of Saturday reminders.")
-    elif command == "Sunday":
+    elif command == "sunday":
         for task in sunday_reminders:
             task_number = task_number + 1
             print(str(task_number) + ") " + task)
-        task_deleted = input("Which task would you like to remove: ")
+        with sr.Microphone() as source:
+            print("Which task would you like to remove: ")
+            audio = r.listen(source)
+        try:
+            task_deleted = r.recognize_google(audio)
+        except:
+            print("Could not recognize your voice")
         if len(sunday_reminders) >= int(task_deleted):
             print("I have removed task " + task_deleted + " from your reminders on Sunday.")
-            sunday_reminders.pop(int(task_deleted - 1))
+            sunday_reminders.pop(int(task_deleted) - 1)
         else:
             print("That task is not in range of Sunday reminders.")
     else:
@@ -214,25 +270,31 @@ while True:
             print("Here are your reminders for today:")
             show_reminders_today()
             current_date = calendar.day_name[todays_date.weekday()]
-    command = input()
+    with sr.Microphone() as source:
+        print("Say your command: ")
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+    except:
+        print("Could not recognize your voice")
     if command == "end":
         break
-    elif command == "How is the weather?":
+    elif command == "how is the weather":
         get_weather()
         continue
-    elif command == "What is the date?":
+    elif command == "what is the date":
         get_date()
         continue
-    elif command == "Set a reminder":
+    elif command == "set a reminder":
         make_reminder()
         continue
-    elif command == "Show me my reminders for today":
+    elif command == "show me my reminders for today":
         show_reminders_today()
         continue
     elif "Show me my reminders for" in command:
         show_reminders_date()
         continue
-    elif "Remove task" in command:
+    elif "remove task" in command:
         remove_task()
         continue
     else:
